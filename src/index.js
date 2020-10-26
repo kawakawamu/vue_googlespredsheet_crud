a58244ad7a53d4c549bdc2870faa6cdc863efb12
河村 泰佑 <kata.kawa813@gmail.com>
2020-10-26 22:58:10 +0900

    service workerについて

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('this is console.index.js');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service_worker.js', { scope: '/' })
+    .then(function (reg) {
+      console.log('登録に成功しました Scopeは' + reg.scope);
+    }).catch(function (error) {
+      console.log('登録に成功しました Scopeは' + error);
+    });
+}
--- src/service_worker.js
+++ src/service_worker.js
@@ -1,14 +1,7 @@
-let CACHE_NAME = 'pwa-sample-caches';
-let urlsToCache = [
+self.addEventListener('install', (event) => {
+  console.log('service worker install');
+});
 
-];
-
-self.addEventListener('install', function (event) {
-  event.waitUntll(
-    caches
-      .open(CACHE_NAME)
-      .then(function (cache)) {
-    return cache.addAll(urlsToCache);
-  }
-  );
+self.addEventListener('active', (event) => {
+  console.info('active', event);
 });
