224910c5bcb94cc87afa506827c8418fd280bf64
河村 泰佑 <kata.kawa813@gmail.com>
2020-11-08 23:23:20 +0900

    VueCli install

--- src/index.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service_worker.js', { scope: '/' })
-    .then(function (reg) {
-      console.log('登録に成功しました Scopeは' + reg.scope);
-    }).catch(function (error) {
-      console.log('登録に成功しました Scopeは' + error);
-    });
-}
--- src/service_worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('service worker install');
-});
-
-self.addEventListener('active', (event) => {
-  console.info('active', event);
-});
