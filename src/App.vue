3e96e47ef429c386522c646b2583b742ac6bbde6
河村 泰佑 <kata.kawa813@gmail.com>
2020-11-09 00:14:57 +0900

    ヘッダー部分

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="blue" dark>
+      <v-toolbar-title>comment</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon to="/">
+        <v-icon>mdi-apple</v-icon>
+      </v-btn>
+      <v-btn icon to="/settings">
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
+  components: {},
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
