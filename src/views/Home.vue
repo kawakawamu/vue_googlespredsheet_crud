ba1a58a103166d97141a8ebe4539afe72f774c1e
河村 泰佑 <kata.kawa813@gmail.com>
2020-11-10 22:33:31 +0900

    Homeコンポーネント部分

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,43 @@
 <template>
-  <div class="home">
-    <h1>Homeコンポーネントです</h1>
+  <div>
+    <v-card>
+      <v-card-title>
+        <v-col cols="8">
+          <v-menu
+            ref="menu"
+            v-model="menu"
+          >
+            <template>
+              <v-text-field
+                readonly
+                hide-details
+              />
+            </template>
+            <v-date-picker
+              color="red"
+              locale="ja-jp"
+              no-title
+              scrollable
+            >
+            </v-date-picker>
+          </v-menu>
+        </v-col>
+      </v-card-title>
+    </v-card>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "Home",
+
+  data() {
+    return {
+      loading: false,
+      menu: false,
+      search: ''
+    }
+  }
 };
 </script>
