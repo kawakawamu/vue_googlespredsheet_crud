e0cf8869732337f453bf177c3c4ec5cb450ff891
河村 泰佑 <kata.kawa813@gmail.com>
2020-11-11 21:20:26 +0900

    画面2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,41 +3,30 @@
     <v-card>
       <v-card-title>
         <v-col cols="8">
-          <v-menu
-            ref="menu"
-            v-model="menu"
-          >
+          <v-menu ref="menu" v-model="menu">
             <template>
-              <v-text-field
-                readonly
-                hide-details
-              />
+              <v-text-field readonly hide-details />
             </template>
-            <v-date-picker
-              color="red"
-              locale="ja-jp"
-              no-title
-              scrollable
-            >
+            <v-date-picker color="red" locale="ja-jp" no-title scrollable>
             </v-date-picker>
           </v-menu>
         </v-col>
       </v-card-title>
+      <v-card-tabel class="text-no-wrap" :headers="tableHeaders">
+      </v-card-tabel>
     </v-card>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Home",
-
   data() {
     return {
       loading: false,
       menu: false,
-      search: ''
-    }
-  }
+      search: "",
+    };
+  },
 };
 </script>
