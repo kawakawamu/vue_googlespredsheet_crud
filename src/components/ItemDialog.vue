5fd2bc47adb9c194432c95865abd2eaf7d4490f9
河村 泰佑 <kata.kawa813@gmail.com>
2020-11-17 21:05:06 +0900

    ItemDialog.vue作成

---
+++ src/components/ItemDialog.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "ItemDialog",
+
+  data() {
+    return {
+      /** ダイアログの表示状態 */
+      show: false,
+      /** 入力したデータが有効かどうか */
+      valid: false,
+      /** 日付選択メニューの表示状態 */
+      menu: false,
+      /** ローディング状態 */
+      loading: false,
+
+      /** 操作タイプ 'add' or 'edit' */
+      actionType: "add",
+      /** id */
+      id: "",
+      /** 日付 */
+      date: "",
+      /** タイトル */
+      title: "",
+      /** 収支 'income' or 'outgo' */
+      inout: "",
+      /** カテゴリ */
+      category: "",
+      /** タグ */
+      tags: [],
+      /** 金額 */
+      amount: 0,
+      /** メモ */
+      memo: "",
+    };
+  },
+};
+</script>
