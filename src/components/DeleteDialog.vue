e24f066bbf0fd493a4338e84fa4fb760a645f9ce
河村 泰佑 <kata.kawa813@gmail.com>
2020-11-29 20:39:37 +0900

    add DeleteDialog

--- src/components/DeleteDialog.vue
+++ src/components/DeleteDialog.vue
@@ -2,5 +2,16 @@
   <v-dialog v-model="show"> </v-dialog>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      show: false,
+      item: {},
+    };
+  },
+
+  computed: mapState({
+    loading: (state) => state.loading.delete,
+  }),
+};
 </script>
