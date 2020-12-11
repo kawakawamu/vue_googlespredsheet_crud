85f274eeb4853fe545a6f6ac525f1ea51b91b1e1
河村 泰佑 <kata.kawa813@gmail.com>
2020-12-11 21:02:07 +0900

    add Setting

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    <h5>Settingsページ</h5>
+  <div class="form-wrapper">
+    <v-form v-model="vaild">
+      <h5>Settingsページ</h5>
+      <v-text-field label="アプリ名" />
+    </v-form>
   </div>
 </template>
 
 <script>
 export default {
   name: "Settings",
+
+  data() {
+    return {
+      valid: false,
+    };
+  },
 };
 </script>
