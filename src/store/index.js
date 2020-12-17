9746f6c83aecbee4c9aa973a35a4468cc6dd88e2
河村 泰佑 <kata.kawa813@gmail.com>
2020-12-17 21:49:04 +0900

    setting

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,22 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const state = {
+  abData: {},
+
+  loading: {
+    fetch: false,
+    add: false,
+    update: false,
+    delete: false,
+  },
+
+  errorMessage: "",
+
   settings: {
     appName: "家計簿！",
+    appUrl: "",
+    authToken: "",
+    strIncomeItems: "",
   },
 };
 
