d829a265749cbf7e8b3c81b06d390addf038c762
河村 泰佑 <kata.kawa813@gmail.com>
2020-12-23 20:57:29 +0900

    add vuelist

--- src/api/gasApi.js
+++ src/api/gasApi.js
@@ -3,3 +3,15 @@ import axios from "axios";
 const gasApi = axios.create({
   headers: { "content-type": "application/x-www-form-urlencoded" },
 });
+
+gasApi.interceptors.response.use(
+  (res) => {
+    if (res.data.error) {
+      return Promise.reject(res.data.error);
+    }
+    return Promise.resolve(res);
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
