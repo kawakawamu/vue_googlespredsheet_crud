ddcfd6d575c2b6c9224e6f2df6a5d1bc7e91e4f2
河村 泰佑 <kata.kawa813@gmail.com>
2021-01-03 19:17:59 +0900

    いろいろ修正

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import gasApi from "../api/gasApi";
 
 Vue.use(Vuex);
 
@@ -37,6 +36,13 @@ const mutations = {
       list.push(item);
     }
   },
+  updateAbData(state, { yearMonth, id }) {
+    const list = state.abData[yearMonth];
+    if (list) {
+      const index = list.findIndex((v) => v.id === item.id);
+      list.splice(index, 1, item);
+    }
+  },
   deleteAbData(state, { yearMonth, id }) {
     const list = state.abData[yearMonth];
     if (list) {
@@ -55,6 +61,23 @@ const mutations = {
   saveSettings(state, { message }) {
     state.settings = { ...settings };
     const { appName, apiUrl, authToken } = state.settings;
+    document.title = appName;
+    gasApi.setUrl(apiUrl);
+    gasApi.setAuthToken(authToken);
+    state.abData = {};
+
+    localStorage.setItem("settings", JSON.stringify(settings));
+  },
+
+  loadSettings(state) {
+    const settings = JSON.parse(localStorage.getItem("settings"));
+    if (settings) {
+      state.settings = Object.assign(state.settings, settings);
+    }
+    const { appName, apiUrl, authToken } = state.settings;
+    document.title = appName;
+    gasApi.setUrl(apiUrl);
+    gasApi.setAuthToken(authToken);
   },
 };
 
